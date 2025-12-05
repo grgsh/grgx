3e86e989216614d359949dc211ac9342e69ca94b
Grzegorz Ilnicki <dev@grg.sh>
2025-12-05 14:28:43 +0100

    fix: correct directory references

--- bin/grgx-init.ps1
+++ bin/grgx-init.ps1
@@ -2,7 +2,7 @@ $GrgxRoot = Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Defin
 
 Import-Module (Join-Path $GrgxRoot "lib/GrgxConfig.psm1")
 Import-Module (Join-Path $GrgxRoot "lib/GrgxUtilities.psm1")
-Import-Module (Join-Path $GrgxRoot "lib/GrgxCompletions.psm1")
+Import-Module (Join-Path $GrgxRoot "completions/GrgxTabCompletions.psm1")
 
 Register-ArgumentCompleter -CommandName grgx, grgx.ps1 -ScriptBlock {
         Get-GrgxCompletions $commandName $parameterName $wordToComplete $commandAst $fakeBoundParameters
--- lib/GrgxConfig.psm1
+++ lib/GrgxConfig.psm1
@@ -1,8 +1,10 @@
-$Script:DefaultModulesDirectory = Join-Path $PSScriptRoot "modules"
+$Script:GrgxRoot = Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$Script:DefaultModulesDirectory = Join-Path $Script:GrgxRoot "plugins"
 $Script:ModulesDirectory = if ($env:GRGX_DIR) { $env:GRGX_DIR } else { $Script:DefaultModulesDirectory }
-$Script:LogFile = Join-Path $PSScriptRoot "grgx.log"
+$Script:LogFile = Join-Path $Script:GrgxRoot "grgx.log"
 
 $Script:Config = @{
+        GrgxRoot                = $Script:GrgxRoot
         ModulesDirectory        = $Script:ModulesDirectory
         DefaultModulesDirectory = $Script:DefaultModulesDirectory
         LogFile                 = $Script:LogFile
