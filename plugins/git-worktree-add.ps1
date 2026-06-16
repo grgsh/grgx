bb03e9d7eb44a327423dee17935108cb029c99d4
Grzegorz Ilnicki <dev@grg.sh>
2026-06-16 18:46:09 +0200

    feat: enhance git worktree add script to check for existing branches

--- plugins/git-worktree-add.ps1
+++ plugins/git-worktree-add.ps1
@@ -7,6 +7,13 @@ $branch = "$name"
 
 git fetch origin
 
-git worktree add $branch `
-    -b $branch `
-    --no-track "origin/$base"
+# Check if branch exists on remote
+if (git ls-remote --heads origin $branch) {
+    # Add worktree for existing branch
+    git worktree add $branch "origin/$branch"
+} else {
+    # Create new branch based on base branch
+    git worktree add $branch `
+        -b $branch `
+        --no-track "origin/$base"
+}
